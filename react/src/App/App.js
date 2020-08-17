0bcf0979cc54ea55af8c08692adb1b670fa30675
bubz <bao.es.hoang2@gmail.com>
2020-08-17 18:40:46 +1000

    trying to do the thing

--- react/src/App/App.js
+++ react/src/App/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
+
+// component imports
+import Sample from '../Components/Sample'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < Sample/>
+
+      {/* put nav into its own component */}
+      <Router>
+      <ul>
+        <li><Link to="/">main</Link></li>
+        <li><Link to="/list">list</Link></li>
+      </ul>
+      {/* routing */}
+
+        <div>
+          {/* <Route exact path="/" component={Main} />
+          <Route path="/list" component={List} /> */}
+        </div>
+        
+      </Router>
+
     </div>
   );
 }
