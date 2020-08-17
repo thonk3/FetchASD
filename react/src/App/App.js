25668ce37a0fbaf711a552c211179e76e79a7ed2
bubz <bao.es.hoang2@gmail.com>
2020-08-17 20:35:20 +1000

    pls

--- react/src/App/App.js
+++ react/src/App/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { 
+  BrowserRouter as Router, 
+  // Route, 
+  Link 
+} from 'react-router-dom'
 import './App.css';
 
 // component imports
