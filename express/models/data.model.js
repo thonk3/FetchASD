9dedfb5b27097edb69ae02696777956955290d8f
Jason Ko <ns0j@protonmail.com>
2020-08-31 12:09:34 +1000

    Added initial schema for DogDates and Friends

---
+++ express/models/data.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { schema } = require('./canine.model');
+const Schema = mongoose.Schema;
+
+const dogDateSchema = new Schema ({
+    firstDogID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Canine'
+    },
+    secondDogID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'Canine'
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ["Requested", "Upcoming", "Completed"]
+    },
+    dateOn: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    parkID: {
+        type: Schema.Types.ObjectId,
+        required: false,
+    },
+    location: {
+        type: String,
+        required: true
+    }
+});
+
+const DogDate = mongoose.model('DogDate', dogDateSchema);
+
+module.exports = DogDate;
