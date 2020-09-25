aaadb1ec332ab8c1847ec3b004f90eebe4d61c99
bubz <bao.es.hoang2@gmail.com>
2020-09-25 21:39:05 +1000

    cleaning up some context stuff in App.js

--- react/src/App/App.js
+++ react/src/App/App.js
@@ -10,52 +10,41 @@ import {
 import NavBar from '../Common/NavBar/NavBar'
 import * as Routes from '../Routes/Routes'
 import PrivateRoute from './PrivateRoute'
+import Footer from '../Common/Footer/Footer';
 
 // material ui
 import useStyles from './App.style';
-import { Button } from '@material-ui/core';
-
 import { useAuth } from '../Context/authContext'
 
 
 const App = (props) => {
   const classes = useStyles();
 
-  // use auth context 
-  // see provider in AppWrapper
-  const { loggedIn } = useAuth();
-
   // demo nonsense
 
   return (
     <BrowserRouter>
-        <NavBar authState={loggedIn} />
-        <div className={classes.offset}></div>
+      {/* nav */}
+      <NavBar />
+      <div className={classes.offset}></div>
 
-        {/* <div> */}
-        <RouterSwitch>
-          {/* setup isLoggedin bool for this to redirect to kennel if logged in */}
-          <Route exact path='/' component={() => <Routes.Home loggedIn={loggedIn} />} />
-          <Route path='/login' component={Routes.Login} />
-          <Route path='/register' component={Routes.Register} />
+      {/* content */}
+      <RouterSwitch>
+        <Route exact path='/' component={Routes.Home} />
+        <Route path='/login' component={Routes.Login} />
+        <Route path='/register' component={Routes.Register} />
           
-          {/* only logged in users can see these */}
-          
-          <PrivateRoute path='/myacc/mypack' component={Routes.DogMan} />
-          <PrivateRoute path='/myacc' component={Routes.AccountMan} />
-          {/* <PrivateRoute path='/kennel' component={Routes.Kennel} /> might be removed/ combine with home */}
-          <PrivateRoute path='/date' component={Routes.Dates} />
-          <PrivateRoute path='/date/id' component={Routes.RateDate} />
-          <PrivateRoute path='/admin' component={Routes.AdminHome} />
+        {/* only logged in users can see these */}
+        <PrivateRoute path='/myacc/mypack' component={Routes.DogMan} />
+        <PrivateRoute path='/myacc' component={Routes.AccountMan} />
+        <PrivateRoute path='/date' component={Routes.Dates} />
+        <PrivateRoute path='/date/id' component={Routes.RateDate} />
+        <PrivateRoute path='/admin' component={Routes.AdminHome} />
+        <PrivateRoute path='/:id' component={Routes.Dog} />
 
-          <PrivateRoute path='/:id' component={Routes.Dog} />
-
-
-          <Route component={Routes.NotFound} />
-
-        </RouterSwitch>
-
-        <p>Footer component</p>
+        <Route component={Routes.NotFound} />
+      </RouterSwitch>
+      <Footer />
     </BrowserRouter>
   )
 };
---
+++ react/src/Common/Footer/Footer.js
@@ -0,0 +1,19 @@
+/* 
+    Footer component for every page
+*/
+
+import React from 'react'
+
+
+
+const Footer = props => {
+
+
+
+
+    return (
+        <h3>This is footer</h3>
+    );
+};
+
+export default Footer;
