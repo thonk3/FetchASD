07fb0bd9b702c7be3c2f1584d8c9704782ef6ae4
Max Caminer <maxcaminer1@gmail.com>
2020-09-27 21:57:35 +1000

    Small Visual fix in Create Dog

--- react/src/Routes/DogMan/DogMan.js
+++ react/src/Routes/DogMan/DogMan.js
@@ -45,7 +45,7 @@ class DogMan extends Component {
         return (
             <div>
                 <Paper style={{ margin: "10px", padding: "10px" }}>
-                    <Typography component="h1" variant="h4" align="center">
+                    <Typography component="h1" variant="h4" align="center" style={{margin: "10px"}}>
                     <span role="img" aria-labelledby="dog">🐶</span> My Pack <span role="img" aria-labelledby="dog">🐶</span>
                     </Typography>
                     <Grid container spacing={2} style={{ marginLeft: 5 }}>
@@ -54,7 +54,7 @@ class DogMan extends Component {
                     <Box style={{ display: "flex", justifyContent: "center", margin: "1vw" }}>
                         <Link to={'/myacc/mypack/newdog'} Component={CreateDog}>
 
-                            <Button style={{ width: "300px" }} type="submit" variant="contained" color="primary">
+                            <Button style={{ width: "300px", margin: "10px" }} type="submit" variant="contained" color="primary">
                                 Create New Dog
                             </Button>
                         </Link>
