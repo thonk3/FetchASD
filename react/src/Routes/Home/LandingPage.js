b8770754a4f6bca50a034569235a00088f2aa991
Amara <amaratut2000@gmail.com>
2020-09-28 00:28:44 +1000

    fixed home page and footer

--- react/src/Routes/Home/LandingPage.js
+++ react/src/Routes/Home/LandingPage.js
@@ -1,20 +1,14 @@
 import React from 'react';
-
 import LandingHero from './LandingComponents/Hero/LandingHero';
-import LandingValues from './LandingComponents/Values/LandingValues'
-import LandingCategories from './LandingComponents/Categories/LandingCategories';
+import LandingCategories from './LandingComponents/Categories/LandingCategories'
 import LandingHowItWorks from './LandingComponents/HowItWorks/LandingHowItWorks';
 import LandingHelp from './LandingComponents/Help/LandingHelp';
 
-
-// I DONT KNOW WHAT ANY OF THIS DOES
-// HOW DO I REFACTOR THIS
 const LandingPage = () => {
     return (
         <>
             <LandingHero />
-            <LandingValues />
-            <LandingCategories />
+            <LandingCategories/>
             <LandingHowItWorks />
             <LandingHelp />
         </>
