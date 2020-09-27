3f5c310b346118d2c95e5591ab5a6c123bf1ed34
bubz <bao.es.hoang2@gmail.com>
2020-09-28 07:59:16 +1000

    updated run script

--- express/thing.js
+++ express/thing.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
-    res.json({ message: "pass!" });
+    return res.json({ message: "pass!" });
 })
 
 module.exports = router;
