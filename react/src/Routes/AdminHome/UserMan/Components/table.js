00837405ce6fb0bba81b3d875807f58d1ea18c12
bubz <bao.es.hoang2@gmail.com>
2020-10-20 06:04:48 +1100

    better now

--- react/src/Routes/AdminHome/UserMan/Components/table.js
+++ react/src/Routes/AdminHome/UserMan/Components/table.js
@@ -1,52 +1,40 @@
+import React from "react";
 
-import React from 'react';
+import TableCell from "@material-ui/core/TableCell";
 
+import TableRow from "@material-ui/core/TableRow";
 
-import TableCell from '@material-ui/core/TableCell';
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import User from "./UserPage";
 
-import TableRow from '@material-ui/core/TableRow';
-
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-import User from './UserPage';
-
-
-const DogList = ({dogList=[]}) => {
-
+const DogList = ({ dogList = [] }) => {
   return (
     <>
-    
-               
-    { dogList.map((data,index) => {
+      {dogList.map((data, index) => {
         if (data) {
           return (
-              <div>
-
-
- 
-                  <TableRow key={data.firstName}>
-                    <TableCell>
-                    {data.firstName}
-                    </TableCell>
-                    <TableCell>{data.email}</TableCell>
-                    <TableCell>Delete</TableCell>
-                    <TableCell><Link to={data._id} Component={User}>
-                    <Button  variant="contained" color="primary" > View Profile </Button>
-                    </Link></TableCell>
-                  </TableRow>
-
-            
-
-         
-          
-	    </div>	
-     
-    	   )	
-    	 }
-    	 return null
-    }) }
+            <div>
+              <TableRow key={data.firstName}>
+                <TableCell>{data.firstName}</TableCell>
+                <TableCell>{data.email}</TableCell>
+                <TableCell>Delete</TableCell>
+                <TableCell>
+                  <Link to={data._id} Component={User}>
+                    <Button variant="contained" color="primary">
+                      {" "}
+                      View Profile{" "}
+                    </Button>
+                  </Link>
+                </TableCell>
+              </TableRow>
+            </div>
+          );
+        }
+        return null;
+      })}
     </>
   );
-}
+};
 
-export default DogList 
+export default DogList;
