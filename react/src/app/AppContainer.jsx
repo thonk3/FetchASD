fddf6f7a20932207df6b3390e08880b17ef624b1
debubz <bao.es.hoang2@gmail.com>
2021-01-30 17:40:15 +1100

    AppContainer only needed some docs update?

--- react/src/app/AppContainer.jsx
+++ react/src/app/AppContainer.jsx
@@ -1,33 +1,38 @@
-/* 
-    Wrapper for the logic for App Component
-*/
+
 import App from "./App";
 import React, { useState } from 'react'
 
 import { AuthContext } from '../contexts/authContext'
 
+/* 
+    Wrapper for the logic for App Component
+
+    sets up context provider for autherization status
+*/
 
 function AppWrapper(props) {
-    // TOKEN CONTEXT
+    // token context - empty string or token string
     const existingTokens = JSON.parse(localStorage.getItem("tokens"));
     const [ authTokens, setAuthTokens ] = useState(existingTokens);
-
-    // LOGGED IN CONTEXT
+    // authentication state - booolean
     const [ loggedIn, setLoggedIn ] = useState(existingTokens != null);
 
-    // set new tokens through context
+    // set new token using context
+    // used in Login and LogOut(nav)
     const setTokens = (token) => {
         localStorage.setItem("tokens", JSON.stringify(token));
         setAuthTokens(token);
-        console.log({'tk': (token === null)});
+        // console.log({'tk': (token === null)});
     }
 
     return (
         <AuthContext.Provider 
             value={{ 
-                authTokens, setAuthTokens: setTokens,
-                loggedIn, setLoggedIn,
-                }}
+                authTokens, 
+                setAuthTokens: setTokens,
+                loggedIn, 
+                setLoggedIn,
+            }}
         >   
             <App />
         </AuthContext.Provider>
