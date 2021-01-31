d8aa9b0718ec6a8f36ab76fb146126365d74658c
debubz <bao.es.hoang2@gmail.com>
2021-01-31 22:47:25 +1100

    Route refactoring works now

--- react/src/app/routers/Routes.jsx
+++ react/src/app/routers/Routes.jsx
@@ -13,37 +13,63 @@ import AdminRoute from './AdminRoute'
         user pages      - account required
         admin pages     - account with admin autherization
 */
+const pub = [
+    { type: 'pub', path: '/', component: Pages.Home, exact: true },
+    { type: 'pub', path: '/register', component: Pages.Register, exact: false },
+    { type: 'pub', path: '/login', component: Pages.Login, exact: false },
+    { type: 'pub', path: '/newmsg', component: Pages.NewMsg, exact: false },
+];
+
+const user = [
+    { type: 'user', path: '/myacc/mypack/newdog', component: Pages.CreateDog, exact: false },
+    { type: 'user', path: '/myacc/mypack/:id', component: Pages.UpdateDog, exact: false },
+    { type: 'user', path: '/myacc/mypack', component: Pages.DogMan, exact: false },
+    { type: 'user', path: '/events', component: Pages.Event, exact: false },
+    { type: 'user', path: '/event/create', component: Pages.EventCreate, exact: true },
+    { type: 'user', path: '/event/update/:id', component: Pages.EventUpdate, exact: true },
+    { type: 'user', path: '/event/:id', component: Pages.EventDetails, exact: false },
+    { type: 'user', path: '/myacc', component: Pages.AccountMan, exact: false },
+    { type: 'user', path: '/date', component: Pages.Dates, exact: false },
+    { type: 'user', path: '/inquiries', component: Pages.UserMsg, exact: false },
+];
+
+const admin = [
+    { type: 'admin', path: '/admin/messages', component: Pages.AdminMsg },
+    { type: 'admin', path: '/admin/loc_man/new', component: Pages.AdminLocCreate },
+    { type: 'admin', path: '/admin/loc_man/:id', component: Pages.AdminLocUpdate },
+    { type: 'admin', path: '/admin/loc_man', component: Pages.AdminLocMan },
+    { type: 'admin', path: '/admin/user_man', component: Pages.AdminUserList },
+    { type: 'admin', path: '/admin/:id', component: Pages.AdminUserUpdate },
+]
+
+const misc = [
+    { type: 'dog', path: '/:id', component: Pages.Dog },
+    { type: '404', component: Pages.NotFound },
+]
 
 // --------------------------------------------------------------------------------
 const AppRoutes = () => {
+    let routeList = [
+        ...pub,
+        ...user,
+        ...admin,
+        ...misc
+    ]
     return (
         <Switch>
-            {/* public pages */}
-            <Route exact path='/' component={Pages.Home} />
-            <Route path='/register' component={Pages.Register} />
-            <Route path='/login' component={Pages.Login} />
-            <Route path='/newmsg' component={Pages.NewMsg} />
-            {/* user pages */}
-            <UserRoute path='/myacc/mypack/newdog' component={Pages.CreateDog} />
-            <UserRoute path='/myacc/mypack/:id' component={Pages.UpdateDog} />
-            <UserRoute path='/myacc/mypack' component={Pages.DogMan} />
-            <UserRoute path='/events' component={Pages.Event} />
-            <UserRoute exact path='/event/create' component={Pages.EventCreate} />
-            <UserRoute exact path='/event/update/:id' component={Pages.EventUpdate} />
-            <UserRoute path='/event/:id' component={Pages.EventDetails} />
-            <UserRoute path='/myacc' component={Pages.AccountMan} />
-            <UserRoute path='/date' component={Pages.Dates} />
-            <UserRoute path='/inquiries' component={Pages.UserMsg} />
-            {/* admin pages */}
-            <AdminRoute path='/admin/messages' component={Pages.AdminMsg} />
-            <AdminRoute path='/admin/loc_man/new' component={Pages.AdminLocCreate} />
-            <AdminRoute path='/admin/loc_man/:id' component={Pages.AdminLocUpdate} />
-            <AdminRoute path='/admin/loc_man' component={Pages.AdminLocMan} />
-            <AdminRoute path='/admin/user_man' component={Pages.AdminUserList} />
-            <AdminRoute path='/admin/:id' component={Pages.AdminUserUpdate} />
-            {/* misc pages */}
-            <UserRoute path='/:id' component={Pages.Dog} />
-            <Route component={Pages.NotFound} />
+            {
+                routeList.map((e, i) => {
+                    if (e.type === "pub")
+                        return <Route key={i} exact={e.exact} path={e.path} component={e.component} />
+                    else if (e.type === "user")
+                        return <UserRoute key={i} exact={e.exact} path={e.path} component={e.component} />
+                    else if (e.type === "admin")
+                        return <AdminRoute key={i} exact={e.exact} path={e.path} component={e.component} />
+                    else if (e.type === "dog")
+                        return <UserRoute key={i} path={e.path} component={e.component} />
+                    else return <Route key={i} component={e.component} />
+                })
+            }
         </Switch>
     )
 }
