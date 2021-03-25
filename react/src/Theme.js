39aa7c6ba33c5f69e1fd2e49bf81246c29ab9864
debubz <bao.es.hoang2@gmail.com>
2021-03-26 02:54:47 +1100

    something happened here

--- react/src/Theme.js
+++ react/src/Theme.js
@@ -1,15 +1,15 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const Theme = createMuiTheme({
-    palette: {
-        primary: { main: '#AF0202' },
-        secondary: { main: '#FFB74D' },
-        background: { default: '#FBF2F2' }
-    },
-    spacing: 16,
-    typography: {
-        fontSize: 12
-    }
+  palette: {
+    primary: { main: "#AF0202" },
+    secondary: { main: "#FFB74D" },
+    background: { default: "#FBF2F2" },
+  },
+  spacing: 16,
+  typography: {
+    fontSize: 12,
+  },
 });
 
 export default Theme;
--- react/src/pages/login/Login.style.js
+++ react/src/pages/login/Login.style.js
@@ -1,15 +1,14 @@
-import { makeStyles } from '@material-ui/core/styles'
-
+import { makeStyles } from "@material-ui/core/styles";
 
 // doing styling is trash
 // idk what any of these means
-const useStyles = makeStyles(theme => ({
-    error: {
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        color: theme.palette.primary.main,
-    }
+const useStyles = makeStyles((theme) => ({
+  error: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: theme.palette.primary.main,
+  },
 }));
 
 export default useStyles;
